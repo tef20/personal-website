7aa25daa9f9cedee4c0615a48d14e1bdac0a57b5
Chris Telford <christelford@hotmail.co.uk>
2023-11-06 12:11:39 -0800

    added missing package import for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+// import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 });
