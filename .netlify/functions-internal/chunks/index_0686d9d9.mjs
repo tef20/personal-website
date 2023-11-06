947e9d3ec64b490c288a610ba51de1f5239465de
Chris Telford <christelford@hotmail.co.uk>
2023-11-06 12:24:06 -0800

    build for deploy

---
+++ .netlify/functions-internal/chunks/index_0686d9d9.mjs
@@ -0,0 +1,9 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro_2c8dc02d.mjs';
+import 'clsx';
+import 'html-escaper';
+
+const page = () => import('./pages/index_2e9cde18.mjs').then(n => n.b);
+
+export { page };
--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_25305dbc.mjs';
+import { manifest } from './manifest_4c9ad5f8.mjs';
 import './chunks/astro_2c8dc02d.mjs';
 import 'clsx';
 import 'html-escaper';
@@ -11,14 +11,14 @@ import '@astrojs/internal-helpers/path';
 import 'mime';
 import 'path-to-regexp';
 
-const _page0  = () => import('./chunks/generic_aa0fd1b2.mjs');
-const _page1  = () => import('./chunks/index_d88815db.mjs');
-const _page2  = () => import('./chunks/exampleBlog_73d27ef8.mjs');
-const _page3  = () => import('./chunks/index_6125a660.mjs');
+const _page0  = () => import('./chunks/generic_acffebaa.mjs');
+const _page1  = () => import('./chunks/index_0b828581.mjs');
+const _page2  = () => import('./chunks/exampleBlog_70ec7af1.mjs');
+const _page3  = () => import('./chunks/index_0686d9d9.mjs');
 const _page4  = () => import('./chunks/redegg_31f686d4.mjs');
-const _page5  = () => import('./chunks/rss_e396fecb.mjs');
-const _page6  = () => import('./chunks/index_e855ee7c.mjs');
-const _page7  = () => import('./chunks/_.._60f8c062.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/exampleBlog.astro", _page2],["src/pages/projects/index.astro", _page3],["src/pages/projects/redegg.astro", _page4],["src/pages/rss.xml.js", _page5],["src/pages/blog/index.astro", _page6],["src/pages/blog/[...slug].astro", _page7]]);
+const _page5  = () => import('./chunks/rss_cdedf763.mjs');
+const _page6  = () => import('./chunks/index_da5a7ccb.mjs');
+const _page7  = () => import('./chunks/_.._c5250566.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/exampleBlog.astro", _page2],["src/pages/projects/index.astro", _page3],["src/pages/projects/redegg.astro", _page4],["src/pages/rss.xml.js", _page5],["src/pages/blog/index.astro", _page6],["src/pages/blog/[...slug].astro", _page7]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
